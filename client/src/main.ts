2e8fc11c3548128af33e055b311b69c189b14b6d
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-30 09:53:41 +0200

    Added register form + setup bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BootstrapVue from 'bootstrap-vue'
+import './bootstrap.scss'
+import PortalVue from 'portal-vue'
+
+Vue.use(PortalVue)
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false;
 
---
+++ client/src/views/Register.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="about">
+    <b-container>
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-1" label="Email address:" label-for="input-1">
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+
+          <label for="text-password">Password</label>
+          <b-input
+            type="password"
+            id="text-password"
+            aria-describedby="password-help-block"
+            placeholder="Password"
+            v-model="form.password"
+          ></b-input>
+        </b-form-group>
+
+        <b-form @submit.stop.prevent></b-form>
+        <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+          <b-form-input id="input-2" v-model="form.name" required placeholder="Enter name"></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class Register extends Vue {
+  form = {
+    email: "",
+    password: "",
+    name: ""
+  };
+  show = true;
+
+  onSubmit(evt) {
+    evt.preventDefault();
+    alert(JSON.stringify(this.form));
+  }
+
+  onReset(evt) {
+    evt.preventDefault()
+    // Reset our form values
+    this.form.email = ''
+    this.form.name = ''
+    this.form.password = ''
+
+    this.show = false
+    this.$nextTick(() => {
+      this.show = true
+    })
+  }
+}
+</script>
