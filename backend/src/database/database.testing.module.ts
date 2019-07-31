8f056c7015119b3812a77af58455d664e40e4c58
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-31 10:57:56 +0200

    Created database testing module

---
+++ backend/src/database/database.testing.module.ts
@@ -0,0 +1,21 @@
+import { Module, Global, DynamicModule } from '@nestjs/common';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+function DatabaseOrmModule(): DynamicModule {
+  return TypeOrmModule.forRoot({
+    type: 'mongodb',
+    host: '127.0.0.1',
+    port: 27017,
+    database: 'vaptesting',
+    entities: [join(__dirname, '/../**/**.entity{.ts,.js}')],
+    synchronize: false,
+  });
+}
+
+@Module({
+  imports: [
+    DatabaseOrmModule(),
+  ],
+})
+export class DatabaseModule { }
