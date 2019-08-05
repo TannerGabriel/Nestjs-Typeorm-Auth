a079f4b1da2e187b02880bbeaf0fc25059d6ee20
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-08-05 09:14:40 +0200

    User login E2E test

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -5,6 +5,7 @@ import { UsersModule } from '../src/users/users.module';
 import { CreateUserDto } from '../src/users/dto/create-user.dto';
 import { HttpStatus } from '@nestjs/common';
 import { AuthModule } from '../src/auth/auth.module';
+import { LoginUserDto } from '../src/users/dto/login-user.dto';
 
 describe('UserController (e2e)', () => {
   let app;
@@ -28,7 +29,11 @@ describe('UserController (e2e)', () => {
     password: '!somepassword123!',
   };
 
-  let userId: string;
+  const loginUser: LoginUserDto = {
+    email: 'test1@test.com',
+    password: '!somepassword123!',
+  };
+
   let token: string;
 
   it('Create User', () => {
@@ -40,7 +45,6 @@ describe('UserController (e2e)', () => {
         expect(body.userResponse.email).toEqual(user.email);
         expect(body.userResponse._id).toBeDefined();
         expect(body.userResponse.password).toBeDefined();
-        userId = body.userResponse._id;
         token = body.token.token;
       })
       .expect(HttpStatus.CREATED);
@@ -57,9 +61,27 @@ describe('UserController (e2e)', () => {
       .expect(HttpStatus.OK);
   });
 
-  //   it('Login user', () => {
-  //       return request(app.getHttpServer())
-  //       .post('/auth')
-  //       .set('Accept', 'application/json')
-  //   })
+  it('Login user', () => {
+    return request(app.getHttpServer())
+      .post('/auth')
+      .set('Accept', 'application/json')
+      .send(loginUser)
+      .expect(({ body }) => {
+        expect(body.token).toBeDefined();
+        expect(body.expiresIn).toBeDefined();
+        token = body.token;
+      })
+      .expect(HttpStatus.CREATED);
+  });
+
+  it('Should be authenticated after login', () => {
+    return request(app.getHttpServer())
+      .get(`/users/authstate`)
+      .set('Accept', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(({ text }) => {
+        expect(text).toBe('authenticated');
+      })
+      .expect(HttpStatus.OK);
+  });
 });
